4a853a6efdbe602b28cf61ef87b476281d1bef76
Yasushi Sakai <ysshski@gmail.com>
2020-04-17 21:35:39 -0400

    Updates

---
+++ precache-manifest.f5d20c0c070199738262bc657560acbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed8e926e7af9a50913c6b2f47db0119",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15fdb8bc041fed71b407",
+    "url": "/static/css/main.716ee543.chunk.css"
+  },
+  {
+    "revision": "164d48b1f89a225e7588",
+    "url": "/static/js/2.a2ee48d2.chunk.js"
+  },
+  {
+    "revision": "5ced2f2ce7b79285485a360b83230d1b",
+    "url": "/static/js/2.a2ee48d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15fdb8bc041fed71b407",
+    "url": "/static/js/main.5e5f618a.chunk.js"
+  },
+  {
+    "revision": "3575c04263a32957699e",
+    "url": "/static/js/runtime-main.1c9f825b.js"
+  }
+]);
